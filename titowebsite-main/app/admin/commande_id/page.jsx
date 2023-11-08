209e8bb6f3da86f02d0fc7fddad7e864f1321723
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2023-11-08 11:47:32 +0100

    recup infos commande d'un user

--- titowebsite-main/app/admin/commande_id/page.jsx
+++ titowebsite-main/app/admin/commande_id/page.jsx
@@ -1,58 +1,104 @@
 "use client"
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 import SectionTitle from '@/components/Common/SectionTitle';
-import { useSession } from "next-auth/react";
-function Commande({userId}) {
+import { useSession } from 'next-auth/react';
+
+const getMessages = async () => {
+    try {
+        const res = await fetch(`http://localhost:3000/api/commandes_id`, {
+            method: "GET",
+            cache: "no-store",
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch messages");
+        }
+
+        return res.json();
+    } catch (error) {
+        console.error("Error loading messages: ", error);
+        return [];
+    }
+};
+
+export default function Commande({ userId }) {
     const { data: session } = useSession();
+    const [commandes, setCommandes] = useState([]);
+
+    useEffect(() => {
+        getMessages()
+            .then((data) => {
+                const filteredCommandes = data.commande.filter(
+                    (commande) => commande.email === session?.user?.email
+                );
+                setCommandes(filteredCommandes);
+            })
+            .catch((error) => {
+                console.error("Error fetching commandes:", error);
+            });
+    }, [session]);
 
     return (
+        <section id="portfolio" className="relative z-10 py-4 md:py-5 lg:py-7">
+            <div className="container mx-auto px-4">
+                <div className="text-center mb-4">
+                    <h1 className="text-2xl font-bold">
+                        Bienvenue sur votre page admin {session?.user?.name}
+                    </h1>
+                    <SectionTitle
+                        paragraph="Vous avez accès en temps réel aux demandes des clients"
+                        center
+                        width="full"
+                    />
+                </div>
 
+                <div className="flex flex-col items-center shadow-lg rounded-lg my-6 p-4">
+                    <div className="w-full md:w-3/4 lg:w-1/2">
+                        <div className="flex flex-col space-y-4 mb-4">
+                            <p className="text-gray-700">
+                                <span className="font-bold">Email:</span> {session?.user?.email}
+                            </p>
+                            <p className="text-gray-700">
+                                <span className="font-bold">Id:</span> {session?.user?.id}
+                            </p>
+                            <p className="text-gray-700">
+                                <span className="font-bold">Name:</span> {session?.user?.name}
+                            </p>
+                        </div>
 
-        <section id="portfolio" className="relative z-10 py-16 md:py-20 lg:py-28">
-            <h1 className="text-2xl font-bold mb-4">Bienvenue sur la page admin  {session?.user?.name}</h1>
-            <SectionTitle
-                paragraph="Vous avez accès en temps réel aux demandes des clients"
-                center
-                width="635px"
-            />
-            <div className="container px-4 mx-auto">
-                <TableContainer  className="bg-white shadow-md rounded my-6">
-                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                        <TableHead className="bg-gray-50 dark:bg-gray-800">
-                            <TableRow>
-                                <TableCell>Id</TableCell>
-                                <TableCell >Email</TableCell>
-                                <TableCell >Sound</TableCell>
-                                <TableCell >Date</TableCell>
-                                <TableCell align="right">Id</TableCell>
-                                <TableCell align="right">selectedChoice</TableCell>
-                                <TableCell align="right">selectedStyle</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        <p className="bg-black">Email {session?.user?.email}</p>
-                            <p className="bg-black">Id {session?.user?.id}</p>
-                            <p className="bg-black">Name {session?.user?.name}</p>
-                            <p className="bg-black">Image {session?.user?.commande}</p>
+                        <div className="flex flex-col space-y-4">
+                            <span className="text-gray-700 font-bold">Commandes:</span>
+                            {commandes.map((commande, index) => (
+                                <div key={index} className=" p-3 rounded shadow">
+                                    <p className="font-bold">ID:</p> {commande._id}
+                                    <p className="font-bold">Email:</p> {commande.email}
+                                    <p className="font-bold">Name:</p> {commande.name}
+                                    <p className="font-bold">Etat de la commande :</p> {commande.etat}
 
+                                    <div>
+                                        <p className="font-bold">Selected Choice:</p>
+                                        <ul className="list-disc pl-5">
+                                            {commande.selectedChoice?.map((choice, choiceIndex) => (
+                                                <li key={choiceIndex}>{choice}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <p className="font-bold">Sound:</p>
+                                        <ul className="list-disc pl-5">
+                                            {commande.sound.map((sound, soundIndex) => (
+                                                <li key={soundIndex}>{sound}</li>
+                                            ))}
+                                        </ul>
 
-
-
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     );
-
 }
-
-export default Commande;
--- titowebsite-main/app/api/commandes_id/route.js
+++ titowebsite-main/app/api/commandes_id/route.js
@@ -4,6 +4,7 @@ import User from "@/models/commande_client";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import connectMongoDB from "../../../libs/mongodb";
+import {array} from "yup";
 
 
 // Connectez-vous à la base de données MongoDB
@@ -11,21 +12,11 @@ connectDB();
 
 export async function GET(request) {
     try {
-        // Assurez-vous que l'utilisateur est authentifié et que vous avez son ID ou nom d'utilisateur
-        const userId = request.user.id; // Exemple : obtenir l'ID de l'utilisateur à partir de votre mécanisme d'authentification
-
-        await connectMongoDB(); // Assurez-vous que la connexion MongoDB est établie correctement.
-
-        // Recherchez l'utilisateur en utilisant son ID ou nom d'utilisateur
-        const user = await User.findById(userId); // Remplacez "findById" par la méthode appropriée pour rechercher l'utilisateur
-
-        if (user) {
-            // Vous avez maintenant accès aux informations de l'utilisateur connecté
-            return NextResponse.json({ user }, { status: 200 });
-        } else {
-            return NextResponse.json({ message: "Utilisateur non trouvé" }, { status: 404 });
-        }
-    } catch (error) {
-        return NextResponse.json({ error: "Une erreur s'est produite lors de la recherche de l'utilisateur" }, { status: 500 });
+        await connectMongoDB()
+        const commande = await User.find().select('email sound name selectedChoice etat')
+        return NextResponse.json({commande})
+    }
+    catch (error) {
+        console.log(error)
     }
 }
