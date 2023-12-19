5b858fa4f1de9e799469fc98cff31ea07740b5cd
NymaRz <117542981+NymaRz@users.noreply.github.com>
2023-12-19 15:34:27 +0100

    FirstCommit*2

--- titowebsite-main/app/api/delete_command/route.js
+++ titowebsite-main/app/api/delete_command/route.js
@@ -1,35 +1,49 @@
-import User from "@/models/commande_client";
-import { NextResponse } from "next/server";
-import mongoose from "mongoose";
-import connectMongoDB from "../../../libs/mongodb";
+// pages/api/delete_command/[id].js
 import {getSession} from "next-auth/react";
-
-
-
-
-
-//suppression de la commande dans user
-
+import connectMongoDB from "../../../libs/mongodb";
+import mongoose from "mongoose";
 
 export default async function handler(req, res) {
-    const session = await getSession({req});
+    // Ce gestionnaire ne traitera que les requêtes DELETE. Toute autre méthode renverra une erreur 405 (Méthode non autorisée).
     if (req.method === "DELETE") {
-        const {db} = await connectMongoDB();
-        const collection = db.collection("commande_client");
-        const {id} = req.query;
-        const user = await collection.findOne({_id: mongoose.Types.ObjectId(id)});
-        if (!user) {
-            res.status(404).json({message: "Commande introuvable"});
-            return;
-        }
-        if (user.email !== session.user.email) {
-            res.status(401).json({message: "Non autorisé"});
-            return;
-        }
-        await collection.deleteOne({ _id: id });
+        // Récupère la session de l'utilisateur. Cela est nécessaire pour vérifier si l'utilisateur est authentifié et autorisé à effectuer la suppression.
+        const session = await getSession({req});
+        console.log("ERRROROROR")
+        // Établit une connexion à MongoDB en utilisant une fonction personnalisée importée depuis un autre fichier.
+        await connectMongoDB();
 
-        res.status(200).json({ message: "Commande supprimée" });
+        // Récupère l'ID de la commande à supprimer de l'URL, grâce au routage dynamique de Next.js.
+        const {id} = req.query;
+
+        try {
+            // Convertit l'ID de chaîne en ObjectId MongoDB, nécessaire pour la recherche dans la base de données.
+            const objectId = mongoose.Types.ObjectId(id);
+            // Accède à la collection 'commande_client' de la base de données.
+            const collection = req.db.collection("commande_client");
+
+            // Recherche la commande dans la base de données en utilisant l'ObjectId.
+            const user = await collection.findOne({_id: objectId});
+            // Si la commande n'est pas trouvée, renvoie une réponse 404 (Introuvable).
+            if (!user) {
+                return res.status(404).json({message: "Commande introuvable"});
+            }
+            // Vérifie si l'email associé à la commande correspond à l'email de l'utilisateur connecté.
+            if (user.email !== session.user.email) {
+                // Si les emails ne correspondent pas, renvoie une erreur 401 (Non autorisé).
+                return res.status(401).json({message: "Non autorisé"});
+            }
+
+            // Supprime la commande de la base de données.
+            await collection.deleteOne({_id: objectId});
+            // Renvoie une réponse 200 (OK) avec un message de confirmation.
+            return res.status(200).json({message: "Commande supprimée"});
+        } catch (error) {
+            // En cas d'erreur dans le bloc try, log l'erreur et renvoie une réponse 500 (Erreur interne du serveur).
+            console.error("Erreur lors de la suppression de la commande:", error);
+            return res.status(500).json({message: "Erreur lors de la suppression de la commande"});
+        }
     } else {
-        res.status(405).end();
+        // Si la méthode HTTP n'est pas DELETE, renvoie une erreur 405.
+        return res.status(405).end();
     }
 }
