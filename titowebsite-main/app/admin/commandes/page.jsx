0170ccaa47b667bd32345b872a689ed3977df57d
NymaRz <117542981+NymaRz@users.noreply.github.com>
2023-12-20 09:58:04 +0100

    FirstCommit*3

--- titowebsite-main/app/admin/commandes/page.jsx
+++ titowebsite-main/app/admin/commandes/page.jsx
@@ -4,80 +4,67 @@ import * as React from 'react';
 import {getSession} from 'next-auth/react'
 import SectionTitle from '@/components/Common/SectionTitle';
 import Button from "@mui/material/Button";
-import { useState } from 'react';
-
+import {useState} from 'react';
 
 const getMessages = async () => {
     try {
-      const res = await fetch(`http://localhost:3000/tarifs/api/commande`, {
-        method: "GET",
-        cache: "no-store",
-      });
-
-      if (!res.ok) {
-        throw new Error("Failed to fetch messages");
-      }
-
-      return res.json();
-    } catch (error) {
-      console.log("Error loading messages: ",error);
-    }
-  };
-
-    //supprimer une commande delete_command
-
-const delete_command = async (_id) => {
-    try {
-        const res = await fetch(`http://localhost:3000/api/delete_command/${_id}`, {
-            method: "DELETE",
+        const res = await fetch(`http://localhost:3000/tarifs/api/commande`, {
+            method: "GET",
             cache: "no-store",
         });
 
-        const data = await res.json();
-        console.log(_id);
-
         if (!res.ok) {
-            throw new Error("Failed to delete message");
+            throw new Error("Failed to fetch messages");
         }
 
-        // Mettre à jour l'état de votre composant après la suppression
-        // Par exemple, recharger les commandes après la suppression
-        // (Assurez-vous que vous avez une méthode pour récupérer les commandes mises à jour)
-        // const updatedCommands = await getMessages();
-        // Mettez à jour l'état avec les nouvelles commandes
-        // setCommande(updatedCommands);
-
-        return data; // ou retournez ce que vous préférez après la suppression
+        return res.json();
     } catch (error) {
-        console.log("Error deleting message: ", error);
+        console.log("Error loading messages: ", error);
+    }
+};
+
+//supprimer une commande delete_command
+
+const delete_command = async (id, setCommandes) => {
+    try {
+        const endpoint = `http://localhost:3000/api/commandes/${id}`;
+        const res = await fetch(endpoint, {
+            method: "DELETE",
+        });
+
+        if (res.ok) {
+            setCommandes((prev) => prev.filter((commande) => commande._id !== id));
+        } else {
+            const errorData = await res.json();
+            alert(errorData.message || "Erreur lors de la suppression de la commande");
+        }
+    } catch (error) {
+        console.error("Erreur lors de la suppression de la commande: ", error);
     }
 };
 
 
 //TODO: mettre à jour le status de la commande
 
-  /*  const updateCommande = async (id) => {
-        try {
-          const res = await fetch(`http://localhost:3000/api/update_status`, {
-            method: "PATCH",
-            cache: "no-store",
-          });
+/*  Const updateCommande = async (id) => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/update_status`, {
+          method: "PATCH",
+          cache: "no-store",
+        });
 
-          if (!res.ok) {
-            throw new Error("Failed to update message");
-          }
-
-          return res.json();
-        } catch (error) {
-          console.log("Error updating message: ", error);
+        if (!res.ok) {
+          throw new Error("Failed to update message");
         }
 
-    }
+        return res.json();
+      } catch (error) {
+        console.log("Error updating message: ", error);
+      }
+
+  }
 */
-
-
-
-export default async function BasicTable (){
+export default async function BasicTable() {
     const [isMenuOpen, setMenuOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -85,77 +72,78 @@ export default async function BasicTable (){
     };
 
 
-    const {commande}  = await getMessages();
+    const {commande} = await getMessages();
 
-  return (
-      <>
+    return (
+        <>
 
 
-          <section id="portfolio" className="relative z-10 py-16 md:py-20 lg:py-28">
+            <section id="portfolio" className="relative z-10 py-16 md:py-20 lg:py-28">
 
-              <img
-                  src="/images/hero/shape-01.svg"
-                  alt="illustration"
-                  className="absolute top-0 right-0 z-[2] hidden w-64 lg:w-1/2 lg:block -mt-20 -mr-32"/>
+                <img
+                    src="/images/hero/shape-01.svg"
+                    alt="illustration"
+                    className="absolute top-0 right-0 z-[2] hidden w-64 lg:w-1/2 lg:block -mt-20 -mr-32"/>
 
-              <SectionTitle
-                  title="Bienvenue sur la page admin"
-                  paragraph="Vous avez accès en temps réel aux demandes des clients"
-                  center
-                  width="635px"/>
+                <SectionTitle
+                    title="Bienvenue sur la page admin"
+                    paragraph="Vous avez accès en temps réel aux demandes des clients"
+                    center
+                    width="635px"/>
 
-              <div className="container px-4 mx-auto">
-                  <div className="flex flex-wrap -mx-4">
-                      <div className="w-full mb-12 px-4">
-                          <div className="overflow-x-auto">
-                              <table className="table-auto border-collapse w-full">
-                                  <thead>
-                                  <tr className="rounded-lg text-sm font-medium text-gray-700 text-left"
-                                      style={{fontSize: "0.9674rem"}}>
-                                      <th className="px-4 py-2 bg-gray-200">Email</th>
-                                      <th className="px-4 py-2">Format</th>
-                                      <th className="px-4 py-2">Description</th>
-                                      <th className="px-4 py-2">Type de prestation</th>
-                                      <th className="px-4 py-2">Etat</th>
-                                      <th className="px-4 py-2">Actions</th>
-                                      {/* Ajout d'une colonne pour les actions */}
-                                  </tr>
-                                  </thead>
-                                  <tbody className="text-sm font-normal text-gray-700">
-                                  <img
-                                      src="/images/hero/shape-02.svg"
-                                      alt="illustration"
-                                      className="absolute bottom-0 left-0  hidden w-64 lg:w-1/2 lg:block -mb-20 -ml-32"/>
-                                  {commande.map((commande) => (
-                                      <tr key={commande.id} className="hover:bg-gray-100 border-b border-gray-200 py-4">
-                                          <td className="px-4 py-2">{commande.email}</td>
-                                          <td className="px-4 py-2">{commande.selectedChoice}</td>
-                                          <td className="px-4 py-2">{commande.sound}</td>
-                                          <td className="px-4 py-2">{commande.telephone}</td>
-                                          <td className="px-4 py-2">{commande.date}</td>
-                                          <td className="px-4 py-2">{commande.etat}</td>
-                                          <td className="flex px-4 py-2 space-x-2">
-                                              <Button variant="contained"
-                                                      className="bg-red-500 hover:bg-red-600 text-white"
-                                                      onClick={() => delete_command(commande._id)}>
-                                                  Supprimer
-                                              </Button>
-                                              <Button variant="contained"
-                                                      className="bg-green-500 hover:bg-green-600 text-white"
-                                                      onClick={() => updateCommande(commande.id)}>
-                                                  Valider
-                                              </Button>
-                                          </td>
-                                      </tr>
-                                  ))}
-                                  </tbody>
-                              </table>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-      </>
+                <div className="container px-4 mx-auto">
+                    <div className="flex flex-wrap -mx-4">
+                        <div className="w-full mb-12 px-4">
+                            <div className="overflow-x-auto">
+                                <table className="table-auto border-collapse w-full">
+                                    <thead>
+                                    <tr className="rounded-lg text-sm font-medium text-gray-700 text-left"
+                                        style={{fontSize: "0.9674rem"}}>
+                                        <th className="px-4 py-2 bg-gray-200">Email</th>
+                                        <th className="px-4 py-2">Format</th>
+                                        <th className="px-4 py-2">Description</th>
+                                        <th className="px-4 py-2">Type de prestation</th>
+                                        <th className="px-4 py-2">Etat</th>
+                                        <th className="px-4 py-2">Actions</th>
+                                        {/* Ajout d'une colonne pour les actions */}
+                                    </tr>
+                                    </thead>
+                                    <tbody className="text-sm font-normal text-gray-700">
+                                    <img
+                                        src="/images/hero/shape-02.svg"
+                                        alt="illustration"
+                                        className="absolute bottom-0 left-0  hidden w-64 lg:w-1/2 lg:block -mb-20 -ml-32"/>
+                                    {commande.map((commande) => (
+                                        <tr key={commande.id}
+                                            className="hover:bg-gray-100 border-b border-gray-200 py-4">
+                                            <td className="px-4 py-2">{commande.email}</td>
+                                            <td className="px-4 py-2">{commande.selectedChoice}</td>
+                                            <td className="px-4 py-2">{commande.sound}</td>
+                                            <td className="px-4 py-2">{commande.telephone}</td>
+                                            <td className="px-4 py-2">{commande.date}</td>
+                                            <td className="px-4 py-2">{commande.etat}</td>
+                                            <td className="flex px-4 py-2 space-x-2">
+                                                <Button variant="contained"
+                                                        className="bg-red-500 hover:bg-red-600 text-white"
+                                                        onClick={() => delete_command(commande._id)}>
+                                                    Supprimer
+                                                </Button>
+                                                <Button variant="contained"
+                                                        className="bg-green-500 hover:bg-green-600 text-white"
+                                                        onClick={() => updateCommande(commande.id)}>
+                                                    Valider
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
 
-  );
+    );
 }
--- titowebsite-main/app/api/delete_command/route.js
+++ titowebsite-main/app/api/delete_command/route.js
@@ -1,49 +1,36 @@
-// pages/api/delete_command/[id].js
-import {getSession} from "next-auth/react";
-import connectMongoDB from "../../../libs/mongodb";
+import { getSession } from "next-auth/react";
 import mongoose from "mongoose";
+import Commande from "@/models/commande_client";
+import connectMongoDB from "@/libs/mongodb";
 
 export default async function handler(req, res) {
-    // Ce gestionnaire ne traitera que les requêtes DELETE. Toute autre méthode renverra une erreur 405 (Méthode non autorisée).
-    if (req.method === "DELETE") {
-        // Récupère la session de l'utilisateur. Cela est nécessaire pour vérifier si l'utilisateur est authentifié et autorisé à effectuer la suppression.
-        const session = await getSession({req});
-        console.log("ERRROROROR")
-        // Établit une connexion à MongoDB en utilisant une fonction personnalisée importée depuis un autre fichier.
+    if (req.method !== "DELETE") {
+        res.setHeader('Allow', ['DELETE']);
+        return res.status(405).end(`Méthode ${req.method} non autorisée`);
+    }
+
+    try {
         await connectMongoDB();
 
-        // Récupère l'ID de la commande à supprimer de l'URL, grâce au routage dynamique de Next.js.
-        const {id} = req.query;
-
-        try {
-            // Convertit l'ID de chaîne en ObjectId MongoDB, nécessaire pour la recherche dans la base de données.
-            const objectId = mongoose.Types.ObjectId(id);
-            // Accède à la collection 'commande_client' de la base de données.
-            const collection = req.db.collection("commande_client");
-
-            // Recherche la commande dans la base de données en utilisant l'ObjectId.
-            const user = await collection.findOne({_id: objectId});
-            // Si la commande n'est pas trouvée, renvoie une réponse 404 (Introuvable).
-            if (!user) {
-                return res.status(404).json({message: "Commande introuvable"});
-            }
-            // Vérifie si l'email associé à la commande correspond à l'email de l'utilisateur connecté.
-            if (user.email !== session.user.email) {
-                // Si les emails ne correspondent pas, renvoie une erreur 401 (Non autorisé).
-                return res.status(401).json({message: "Non autorisé"});
-            }
-
-            // Supprime la commande de la base de données.
-            await collection.deleteOne({_id: objectId});
-            // Renvoie une réponse 200 (OK) avec un message de confirmation.
-            return res.status(200).json({message: "Commande supprimée"});
-        } catch (error) {
-            // En cas d'erreur dans le bloc try, log l'erreur et renvoie une réponse 500 (Erreur interne du serveur).
-            console.error("Erreur lors de la suppression de la commande:", error);
-            return res.status(500).json({message: "Erreur lors de la suppression de la commande"});
+        const session = await getSession({ req });
+        if (!session) {
+            return res.status(401).json({ message: "Non autorisé" });
         }
-    } else {
-        // Si la méthode HTTP n'est pas DELETE, renvoie une erreur 405.
-        return res.status(405).end();
+
+        const { id } = req.query;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Format d'ID invalide" });
+        }
+
+        const objectId = mongoose.Types.ObjectId(id);
+        const deletedCommande = await Commande.findByIdAndDelete(objectId);
+        if (!deletedCommande) {
+            return res.status(404).json({ message: "Commande introuvable" });
+        }
+
+        return res.status(200).json({ message: "Commande supprimée avec succès" });
+    } catch (error) {
+        console.error("Erreur lors de la suppression de la commande:", error);
+        return res.status(500).json({ message: "Erreur serveur" });
     }
 }
